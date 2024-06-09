353ffa12418df130a55fad053dc8ff514a6fbb80
“Elena <bachi.elena@gmail.com>
2024-06-09 14:16:18 +0200

    Добавлены стили для страницы с активностями

--- src/components/elements/activities-list/_Activities-list.jsx
+++ src/components/elements/activities-list/_Activities-list.jsx
@@ -8,6 +8,12 @@ function ActivitiesList() {
     <section className="activities">
       <div className="wrap">
         <h2 className="activities__title">Чем заняться</h2>
+        <p className="activities__description">
+          Наши загородные домики - идеальное место для проведения активного
+          отдыха на свежем воздухе. <br /> В Klever Village вы найдете
+          интересные и разнообразные развлечения, чтобы сделать ваш отдых
+          незабываемым и полным приключений.
+        </p>
 
         <ul className="activities__list">
           {activities.map((activity) => {
