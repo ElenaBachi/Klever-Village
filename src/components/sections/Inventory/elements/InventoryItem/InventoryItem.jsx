88b4a1eed34801c103ae5a040f0f57a416e1f56e
Elena Bachi <elenabaci@Elenas-MacBook-Air.local>
2024-07-25 10:32:46 +0200

    внесены правки в номерной фонд

--- src/components/sections/Inventory/elements/InventoryItem/InventoryItem.jsx
+++ src/components/sections/Inventory/elements/InventoryItem/InventoryItem.jsx
@@ -4,26 +4,27 @@ import BookButton from "../../../../elements/BookButton/BookButton";
 import "./style.scss";
 
 export default function InventoryItem(props) {
-  const { name, title, description, options, accommodationOptions } = props;
+  const { name, title, description, options, accommodationOptions, prices } =
+    props;
   const isSmall = name === "small-house";
 
   const optionsList = options.map((option, index) => {
     return <li key={index}>{option}</li>;
   });
 
-  const accommodationOptionsList =
-    accommodationOptions.length > 0
-      ? accommodationOptions.map((option, index) => {
-          return (
-            <li key={index}>
-              {option.title}
-              <p className="accommodation__price">
-                стоимость <span>{option.price} &#8381;</span> в сутки
-              </p>
-            </li>
-          );
-        })
-      : "";
+  // const accommodationOptionsList =
+  //   accommodationOptions.length > 0
+  //     ? accommodationOptions.map((option, index) => {
+  //         return (
+  //           <li key={index}>
+  //             {option.title}
+  //             <p className="accommodation__price">
+  //               стоимость <span>{option.price} &#8381;</span> в сутки
+  //             </p>
+  //           </li>
+  //         );
+  //       })
+  //     : "";
 
   return (
     <li className="accommodation__item">
@@ -38,19 +39,35 @@ export default function InventoryItem(props) {
           <>
             <ul className="description">
               Варианты размещения:
-              {accommodationOptionsList}
+              {accommodationOptions
+                ? accommodationOptions.map((option, index) => {
+                    return <li key={index}>{option}</li>;
+                  })
+                : ""}
             </ul>
           </>
         ) : (
           <>
-            <p className="accommodation__price">
+            {/* <p className="accommodation__price">
               Стоимость <span>{accommodationOptions.price} &#8381;</span> в
               сутки
-            </p>
+            </p> */}
           </>
         )}
 
-        <BookButton top={0}/>
+        <div className="accommodation__price">
+          {prices.map((price) => {
+            return (
+              <p>
+                {price.title} <span>{price.price} &#8381; в сутки</span>
+              </p>
+            );
+          })}
+        </div>
+
+        <p className="accommodation__note">Завтрак включен в стоимость проживания</p>
+
+        <BookButton top={0} />
       </div>
     </li>
   );
